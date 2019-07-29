016a2c86c35a83f341be47f1130b31e220aada97
myname21-meet <myname21@meet.mit.edu>
2019-07-29 09:40:27 -0400

    whoops

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -8,20 +8,38 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
-
+def add_article(name,topic,rating):
+	knowledge_object = Knowledge(
+		name = name,
+		topic = topic,
+		rating = rating
+		)
+	session.add(knowledge_object)
+	session.commit()
+	
 def query_all_articles():
-	pass
+	articles = session.query(Knowledge).all()
+	print(articles)
 
-def query_article_by_topic():
-	pass
+def query_article_by_topic(topic):
+	types = session.query(Knowledge).filter_by(topic = topic).all()
+	return types
 
-def delete_article_by_topic():
-	pass
+def delete_article_by_topic(topic):
+	delete_dis = session.query(Knowledge).filter_by(topic = topic).delete()
 
 def delete_all_articles():
-	pass
+	death = session.query(Knowledge).delete()
 
-def edit_article_rating():
-	pass
+def edit_article_rating(name, updated_rating):
+	up_me = session.query(Knowledge).filter_by(name = name).first()
+	up_me.rating = updated_rating	
+	session.commit()
+
+delete_all_articles()
+query_all_articles()
+add_article("Tony","Tonies",10)
+query_all_articles()
+edit_article_rating("Tony",7)
+query_all_articles()
+
--- knowledge_model.py
+++ knowledge_model.py
@@ -6,6 +6,11 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
+	__tablename__ = 'info'
+	info_id = Column(Integer, primary_key = True)
+	name = Column(String)
+	topic = Column(String)
+	rating = Column(Integer)
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
@@ -13,5 +18,7 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
+	def __repr__(self):
+		return("If you want to learn about {}"", you should look at the Wikipedia article called {} "
+			"We gave this article a rating of {}"" out of 10!""(Also heres the key in the database:{}(#only for smart ppl))").format(self.topic,self.name,self.rating,self.info_id)
 
-	pass
